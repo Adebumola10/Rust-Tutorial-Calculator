adc68f2587790d1803f5daed88ccb580e880bd66
Adebumola10 <adebumola120@gmail.com>
2024-11-12 19:02:05 +0100

    Updated Rust Calculator Code with more functionalities

--- main.rs
+++ main.rs
@@ -1,17 +1,52 @@
 //A tutorial calculator built using Rust programming.
 
-fn main() {
-    println!("Welcome To Rust Tutorial Calculator.");
+use std::io; //importing the input and output libary.
 
-    let first_calculation:f32 = 50.6;
-    let second_calculation: f32 = 30.6;
-    let _result: f32 = first_calculation / second_calculation;
+fn main () {
+    println!("Welcome to Rust Calculator");
+
+    let mut _first_prompt = String::new();
+
+    println!("Enter first number:");
+
+    io::stdin().read_line(&mut _first_prompt).expect("Failed to read line");
+
+    let _first_prompt_number: f32 = _first_prompt.trim().parse().expect("Please input a number");
+
+    let mut _operator_calculating = String::new();
+
+    println!("Enter operator sign(+, -, /, * , %):");
+
+    io::stdin().read_line(&mut _operator_calculating).expect("Failed to read line");
+
+    let _operator_calculation: String = _operator_calculating.trim().parse().expect("Please enter operator:");
+
+    let mut _second_prompt =String::new();
+
+    println!("Enter second number:");
+
+    io::stdin().read_line(&mut _second_prompt).expect("Failed to read line");
     
-    let _type_casted_first_calcualtion = first_calculation as i32;
-    let _type_casted_second_calculation = second_calculation as i32;
-    let _result_type_casted = _result as i32;
+    let _second_prompt_number: f32 = _second_prompt.trim().parse().expect("Please enter a number:");
 
-    println!("Result = {}", _result);
-    println!("Type_Casted_Result = {}", _result_type_casted);
+    let _result_cal:f32 = match & _operator_calculation as &str {
+        "+" => _first_prompt_number + _second_prompt_number,
+        "-" => _first_prompt_number - _second_prompt_number,
+        "*" => _first_prompt_number * _second_prompt_number,
+        "%" => _first_prompt_number % _second_prompt_number,
+        "/" => {
+            if _second_prompt_number != 0.0 {
+                _first_prompt_number / _second_prompt_number
+            } else {
+                println!("Error: E");
+                return;
+            }
+        }
+        _ => {
+            println!("Error: Inavlid operator. please use +, -, /, *, %");
+            return;
+        }
+    };
 
+    println!("Ans = {}", _result_cal);
 }
